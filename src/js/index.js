05df2eac3c1ff2bb66a5e76defbc7676b0713da3
atumv <atumv.gm@gmail.com>
2022-01-25 13:38:02 +0700

    Fix mobile add button

--- src/js/index.js
+++ src/js/index.js
@@ -1,38 +1,54 @@
 import '../styles/style.scss';
 
+const slickOptions = {
+  infinite: true,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  nextArrow: '.speakers-next-btn',
+  variableWidth: true,
+};
+
 $(document).ready(function () {
-  $('.speakers-container').slick({
-    infinite: true,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    nextArrow: '.speakers-next-btn',
-    variableWidth: true,
-  });
+  $('.speakers-container').slick(slickOptions);
 });
 
 const list = document.querySelector('.list'),
   table = document.querySelector('.table'),
   toggleDescBtn = document.querySelector('.list-event-program-toggle-btn'),
   desc = document.querySelector('.list-event-program-description'),
-  addBtnContainer = document.querySelector('.list-event-add-btn-container'),
-  addBtn = document.querySelector('.list-event-add-btn'),
-  addBtnIcon = document.querySelector('.list-event-add-btn-icon'),
+  addBtnContainers = document.querySelectorAll('.list-event-add-btn-container'),
+  addButtons = document.querySelectorAll('.list-event-add-btn'),
+  addButtonIcons = document.querySelectorAll('.list-event-add-btn-icon'),
   scheduleViewBtn = document.querySelector('.toggle-view-btn'),
   scheduleView = document.querySelector('.schedule-view'),
   addToCalendarBtn = document.querySelectorAll('.add-to-calendar-btn');
 
-toggleDescBtn.addEventListener('click', toggleDescriptionShow);
-addBtnContainer.addEventListener('click', toggleSubscription);
-scheduleViewBtn.addEventListener('click', toggleScheduleView);
 addToCalendarBtn.forEach((btn) =>
   btn.addEventListener('click', toggleAddToCalendar)
 );
+addBtnContainers.forEach((container) =>
+  container.addEventListener('click', toggleSubscription)
+);
+scheduleViewBtn.addEventListener('click', toggleScheduleView);
+toggleDescBtn.addEventListener('click', toggleDescriptionShow);
 
 function toggleAddToCalendar(e) {
   e.target.classList.toggle('add-to-calendar-btn-enabled');
   e.target.classList.contains('add-to-calendar-btn-enabled')
     ? (e.target.innerHTML = 'В календаре')
     : (e.target.innerHTML = 'Добавить <br />в календарь');
+}
+
+function toggleSubscription() {
+  addButtons.forEach((btn) => {
+    btn.classList.toggle('list-event-add-btn-enabled');
+    btn.classList.contains('list-event-add-btn-enabled')
+      ? (btn.innerHTML = 'Добавлено')
+      : (btn.innerHTML = 'Добавить');
+  });
+  addButtonIcons.forEach((icon) =>
+    icon.classList.toggle('list-event-add-btn-enabled')
+  );
 }
 
 function toggleScheduleView() {
@@ -46,12 +62,4 @@ function toggleScheduleView() {
 function toggleDescriptionShow() {
   desc.classList.toggle('hidden');
   toggleDescBtn.classList.toggle('border-bottom-reset');
-}
-
-function toggleSubscription() {
-  addBtn.classList.toggle('list-event-add-btn-enabled');
-  addBtnIcon.classList.toggle('list-event-add-btn-enabled');
-  addBtn.classList.contains('list-event-add-btn-enabled')
-    ? (addBtn.innerHTML = 'Добавлено')
-    : (addBtn.innerHTML = 'Добавить');
 }
